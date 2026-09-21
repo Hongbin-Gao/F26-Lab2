b48073faafa39b9f502e1b77f3c8afed10cc5b79
Hongbin-Gao <hgao51@myseneca.ca>
2026-09-21 15:59:59 +0000

    done lab2a

--- lab2a.py
+++ lab2a.py
@@ -1,13 +1,28 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Hongbin
+# Date: 2026/09/21
 # Purpose: Create a variable, check its type and print the variable.
 # Usage: ./lab2a.py
 
 # TO DO 1: Follow the instructions given in README.md file
 
+# creat a variable "x" and get the value from user
+x = input("Enter a integer: ")
+# print out the type of "x"
+print(type(x))
 
+# convert "x" to integer and assign it again
+x = int(x)
 
+# create if-statement
+# the condition is x need greater than or equal to 6
+if x >= 6:
+    ## while the condition is "TRUE" print out "x is greater then 6!"
+    print("x is greater then 6!")
 
+# the condition is x greater than or equal to 4 and x is less than 12
+elif x >=4 and x < 12:
+    # while the condition is "TRUE" print out appropriate message
+    print("TRUE")
