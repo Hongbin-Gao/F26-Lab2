646c2d1706d6a1f6ab8fb3bc9a585f504af5a435
Hongbin-Gao <hgao51@myseneca.ca>
2026-09-21 16:08:00 +0000

    done lab2b

--- lab2b.py
+++ lab2b.py
@@ -1,11 +1,22 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Hongbin
+# Date: 2026/09/21
 # Purpose: Practice using if and else statments.
 # Usage: ./lab2b.py
 
 # TO DO 1:
 # Follow the instructions given in the README.md file.
 
+# get value from user and assign it to variable "num"
+num = input("Enter a 4 digit integer: ")
+
+# create if-statement
+# print "George Orwell", while the variable "num" is "1984"
+if num == 1984:
+    print("George Orwell")
+
+# otherwise print "Not quite right!"
+else:
+    print("Not quite right!")
